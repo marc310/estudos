ae2eb60c820b35a705bb85a18a9235d395d806f4
marc310 <marcelo@codex>
2019-07-28 15:08:45 -0300

    adicionando repositorio para estudos e alterações pra uso pessoal

--- ionic/devAndroid/src/app/app.module.ts
+++ ionic/devAndroid/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -18,6 +20,7 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule
   ],
   providers: [
+    InAppBrowser,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
--- ionic/devAndroid/src/app/home/home.page.ts
+++ ionic/devAndroid/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,21 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  url: string;
+
+  constructor(private iab: InAppBrowser) {}
+
+    owp( url: string ){
+      const browser = this.iab.create(url, '_self');
+
+        // browser.executeScript(...);
+        //
+        // browser.insertCSS(...);
+        // browser.on('loadstop').subscribe(event => {
+        //    browser.insertCSS({ code: "body{color: red;" });
+        // });
+
+        browser.close();
+    }
 
 }
